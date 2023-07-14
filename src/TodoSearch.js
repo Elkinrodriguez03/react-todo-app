7ddd89d610934e0f9c3f29546e5a68ba53bd373f
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2023-07-13 23:14:39 -0500

    Working on the structure components and styles of To-do app

---
+++ src/TodoSearch.js
@@ -0,0 +1,12 @@
+import './TodoSearch.css'
+
+function TodoSearch() {
+    return (
+        <input 
+            placeholder="Search something..."
+            className="TodoSearch"
+        />
+    )
+}
+
+export { TodoSearch };
